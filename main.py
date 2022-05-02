9d135c804bd2b9e0314e6a5f90cf55e510180850
DenysYuryev <DenysYuryev@gmail.com>
2022-05-02 11:47:04 +0300

    PK004

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class MyWidget(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.ui.TableWidget1.setRowCount(1)
+        self.ui.TableWidget1.setRowCount(0)
         self.ui.TableWidget1.setColumnCount(4)
 
         # style = '''
@@ -29,8 +29,7 @@ class MyWidget(QtWidgets.QMainWindow):
         self.table_index = 0
         self.row_cont = 1
 
-        self.ID = None
-        self.Name = None
+        self.ID = int
         self.Class = None
         self.Dependence = None
         self.Image = None
@@ -50,68 +49,65 @@ class MyWidget(QtWidgets.QMainWindow):
             if (len(self.ui.lineEdit.text())) > 0:
                 self.Name = self.ui.lineEdit.text()
                 print(f"{self.Name}")
-            else:
-                return
 
-            if (len(self.ui.comboBox.currentText())) > 0:
-                self.Class = self.ui.comboBox.currentText()
-                print(f"{self.Class}")
-            else:
-                return
+                if (len(self.ui.comboBox.currentText())) > 0:
+                    self.Class = self.ui.comboBox.currentText()
+                    print(f"{self.Class}")
 
-            if len(self.ui.comboBox_2.currentText()) > 0 and self.ui.radioButton_1.isChecked:
-                self.Dependence = self.ui.comboBox_2.currentText()
-                print(self.Dependence)
-            elif self.ui.radioButton_2.isChecked:
-                self.Dependence = "none"
-                print(f"{self.Dependence}")
+                    if len(self.ui.comboBox_2.currentText()) > 0 and self.ui.radioButton_1.isChecked:
+                        self.Dependence = self.ui.comboBox_2.currentText()
+                        print(f"{self.Dependence}")
+                    elif self.ui.radioButton_2.isChecked:
+                        self.Dependence = "none"
+                        print(f"{self.Dependence}")
+                    else:
+                        print("Return")
+                        return
+                else:
+                    return
             else:
-                print("Return")
                 return
         except Exception as Error:
             res = QtWidgets.QMessageBox.critical(self, 'Error', f"Error: {Error}")
             if res == QtWidgets.QMessageBox.Ok:
                 return
 
-        print(f"row to be added: | {self.Name} | {self.Class} | {self.Dependence} |")
-
+        print(f"Row to be added: | {self.Name} | {self.Class} | {self.Dependence} |")
 
         try:
             i = 0
             index = []
             print("Start count index")
             # print("Item: ", self.ui.TableWidget1.item(i, 0))
+            rowcount = self.ui.TableWidget1.rowCount()
+            print(f"Row count = {rowcount}")
+            if rowcount > 0:
+                for i in rowcount:
+                    index.append(self.ui.TableWidget1.index(i, 0).data())
+                    print(f"Index: {index[i]};")
+                    i += 1
+                i = max(index)
+                print(f"max = {i}")
+            else:
+                i = 0
 
-            while self.ui.TableWidget1.item(i, 0).data() != None:
-                index.append(self.ui.TableWidget1.item(i, 0))
-                print("Index: {};\ni: {};".format(index[i], i))
-                if i >= 10:
-                    break
-                i += 1
+            self.ID = i + 1
+            print("ID: {}".format(self.ID))
 
-            maxim = max(index)
-            print("maxim = ", maxim)
+            self.ui.TableWidget1.setRowCount(self.row_cont)
+            self.ui.TableWidget1.setItem(self.table_index, 0, QtWidgets.QTableWidgetItem(str(self.ID)))
+            self.ui.TableWidget1.setItem(self.table_index, 1, QtWidgets.QTableWidgetItem(self.Name))
+            self.ui.TableWidget1.setItem(self.table_index, 2, QtWidgets.QTableWidgetItem(self.Class))
+            self.ui.TableWidget1.setItem(self.table_index, 3, QtWidgets.QTableWidgetItem(self.Image))
 
-            self.ID = maxim + 1
-
-            print(self.ID)
+            self.table_index += 1
+            self.row_cont += 1
 
         except Exception as Error:
             txt = "Something wrong in -> {} row"
             res = QtWidgets.QMessageBox.critical(self, 'Error', f"Error: {Error}\n" + txt.format(i))
             if res == QtWidgets.QMessageBox.Ok:
                 return
-
-        self.ui.TableWidget1.setRowCount(self.row_cont)
-        self.ui.TableWidget1.setItem(self.table_index, 0, QtWidgets.QTableWidgetItem(str(self.ID)))
-        self.ui.TableWidget1.setItem(self.table_index, 1, QtWidgets.QTableWidgetItem(self.Name))
-        self.ui.TableWidget1.setItem(self.table_index, 2, QtWidgets.QTableWidgetItem(self.Class))
-        self.ui.TableWidget1.setItem(self.table_index, 3, QtWidgets.QTableWidgetItem(self.Image))
-
-        print("Name: " + self.Name + "\n" + "Class: " + self.Name)
-
-        self.table_index += 1
-        self.row_cont += 1
 
     def deleqip(self):
         pass
